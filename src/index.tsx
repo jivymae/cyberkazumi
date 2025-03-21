78e578538608a2a9d5b9646db8e01b63d46b0b65
jivymae <100350935+jivymae@users.noreply.github.com>
2025-03-21 09:54:41 +0800

    Initial Commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
+// src/index.tsx (React 18+)
 import './index.css';
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom/client";
 import { App } from "./App";
-render(<App />, document.getElementById("root"));
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
